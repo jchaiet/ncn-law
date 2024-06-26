b3d7e70db99d2d05ddd8a40e1eb3342f2431db00
Joe Chaiet <jchaiet@hotmail.com>
2024-06-26 08:57:33 -0400

    first commit

--- site/src/components/two-column-block.tsx
+++ site/src/components/two-column-block.tsx
@@ -6,8 +6,6 @@ import * as styles from '../styles/twoColumnBlock.module.scss';
 export default function TwoColumnBlock({ block, raw, index }: { block: any, raw: any, index: Number }) {
   const { image, heading, textPosition } = block;
 
-  console.log(image)
-
   return (
     <div className={styles.twoColumnBlock}>
       <div className={`${styles.container} ${textPosition.toLowerCase() === "start" ? "" : styles.reverse}`}>
